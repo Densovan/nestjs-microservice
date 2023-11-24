fff01e4bb05cf5720059a861ad5b0772a375e955
Sarim Sovanden <sovanden@dr-tech.co>
2023-11-25 01:34:04 +0700

    first commit

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,35 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+// @Module({
+//   imports: [
+//     MongooseModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: (ConfigService: ConfigService) => ({
+//         uri: ConfigService.get('MONGO_URI'),
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+// })
+// export class DatabaseModule {}
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      // imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
